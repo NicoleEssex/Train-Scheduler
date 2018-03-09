7a7488a9bf37f905c65d838a40e5d17bcf728919
Nicole Essex <Essex@Nicole-Essexs-MacBook-Pro-2.local>
2018-03-09 15:12:00 -0700

    attempting Moment JS

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -9,57 +9,57 @@ var config = {
 };
 firebase.initializeApp(config);
 
-// Set Variables
+// Set Firebase Variable
 var database = firebase.database();
-var minAway;
-//set listener for submit button
-$(document).on("click", "#submit-button", function () {
+
+//Set listener for submit button
+$("#submit-button").on("click", function (event) {
 
     event.preventDefault();
-//capture input values 
+
+    
+//Capture input values 
     var trnNm = $("#train").val().trim();
     $("#train").val(""); 
-    console.log(trnNm);
 
     var dstn = $("#dstntn").val().trim();
     $("#dstntn").val("");  
-    console.log(dstn);
 
     var ftt = $("#frstTrnTm").val().trim();
-    $("#frstTrnTm").val("");   
-    console.log(ftt);
+    $("#frstTrnTm").val("");    
+   
 
     var frq = $("#frqncy").val().trim();
     $("#frqncy").val("");  
-    console.log(frq);
 
-    var mnwy = ftt
-
-    //store input values to firebase
+//Store input values to firebase
     database.ref().push({
         trnNm: trnNm,
         dstn: dstn,
         ftt: ftt,
         frq: frq,
-        mnwy: mnwy,
         dateAdded: firebase.database.ServerValue.TIMESTAMP
     });
 });
+
+//Create Firebase event for adding train to the database and adding it to the html table from the user inputs.
 database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
     
-    // storing the snapshot.val() in a variable for convenience
+// Storing the snapshot.val() in a variable
     var sv = snapshot.val();
     
-    // Console.logging the last train's data
+// Console.logging the last train's data
     console.log("FB " + sv.trnNm);
     console.log("FB " + sv.dstn);
     console.log("FB " + sv.ftt);
     console.log("FB " + sv.frq);
     
-    //create new row in table
+
+
+    //create new row in table with variable
     var newRow = $("<tr>");
     
-    //create new columns in table
+    //create new columns in table from snapshot values
     var td1 = $("<td>");
     td1.text(sv.trnNm);
     newRow.append(td1);
@@ -76,11 +76,44 @@ database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", functi
     td4.text(sv.frq);
     newRow.append(td4);
 
-    var td5 = $("<td>");
-    td5.text(sv.mnwy);
-    newRow.append(td5);
+//TO DO:
+// Use MomentsJS to calculate what time the Next Train Arrives based on the frequency of the train and what it's first time was.
+
+// Use Moments JS to calculate how many minutes away the Next Train Arrival time is.
+
+var fttMin = moment.unix(sv.ftt).format('HH:mm','minutes');
+console.log(fttMin);    
+
+    // var timeDif = 
+    //     moment().diff(moment.unix(sv.ftt));
+    //     console.log(timeDif);
+
+    // var nxtRvl = moment().format('HH:mm');
+    // $("#nxtRvl").text(nxtRvl);
+
+    // var mnsWy = $("#mnsWy").val().trim()
+    // // td5.text(sv.nxtRvl);
+    // newRow.append(td5);
+    
+    // var td6 = $("<td>");
+    // td6.text(sv.mnsWy)
+    // newRow.append(td6);
 
     //append all new rows to table
     $("#table").append(newRow);    
 
 });
+var CurrentTime = (moment().format('HH:mm'));
+var NextTrain = ("");
+//practice calling current time and writing to html
+var now = (moment().format('HH:mm'));
+console.log(moment().format('HH:mm'));	
+$("#p").text(now);
+
+// convert now variable to minutes
+var minCon = (moment.unix(now));
+
+
+var breakfast = moment('8:32','HH:mm');
+var lunch = moment('12:52','HH:mm');
+console.log( moment.duration(lunch - breakfast).humanize() + ' between meals' )
